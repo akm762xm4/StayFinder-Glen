e55fa9bdaab688ea25dddc4723e24c1054097850
akm762xm4 <shahfarhan4451@gmail.com>
2025-06-19 15:18:48 +0530

    changes to cors config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,16 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://stay-finder-glen-4ah8.vercel.app/",
+    ],
+    methods: ["GET", "POST", "PUT"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
